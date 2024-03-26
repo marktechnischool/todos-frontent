65d955fb2da5ee36d6e1a5e1e0453871ee2e426a
Marek Dziewit <marek2901@gmail.com>
2024-03-26 14:28:33 +0100

    msw

---
+++ mocks/handlers.js
@@ -0,0 +1,24 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+    http.get('http://mockapi.pzw/todos', () => {
+        return HttpResponse.json([
+            {
+                _id: '1',
+                title: 'Buy milk',
+            },
+            {
+                _id: '2',
+                title: 'Buy eggs',
+            },
+            {
+                _id: '3',
+                title: 'Buy bread',
+            },
+            {
+                _id: '4',
+                title: 'Buy coffee',
+            },
+        ]);
+    }),
+];
